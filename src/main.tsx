fc7d976d46ccd1f5b85a5852af9d952c249762a2
huyu <yhu8836@gmail.com>
2023-06-29 17:58:21 +0800

    sass全局样式

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // 初始化样式
 import "reset-css"
+// UI框架的样式
+
+// 全局样式
+import './assets/styles/global.scss'
 
 import App from './App.tsx'
 
