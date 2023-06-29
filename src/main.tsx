d9402f7063f69a2a0ed07e45f8f9da70c1283f43
huyu <yhu8836@gmail.com>
2023-06-29 17:51:28 +0800

    初始化样式

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// 初始化样式
+import "reset-css"
+
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
