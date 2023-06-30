e2db1aae6bd57745d3b24d98515b9c2edba11bad
huyu <yhu8836@gmail.com>
2023-06-30 10:37:29 +0800

    使用ant design

--- src/components/Comp1/index.tsx
+++ src/components/Comp1/index.tsx
@@ -2,10 +2,14 @@
 
 // 模块化引入
 import styles from "./index.module.scss"
+import {Button} from "antd";
+import {StepBackwardOutlined} from "@ant-design/icons";
 
 const Comp1 = () => {
     return (
         <div className={styles.box}>
+            <Button type="primary">Primary Button</Button>
+            <StepBackwardOutlined style={{fontSize: "40px"}}/>
             <p>Comp1</p>
         </div>
     );
